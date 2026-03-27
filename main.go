92ad97979b42081d81c693247fff2e699b494425
Carlos Gabriel <oditorrinco222@gmail.com>
2026-03-27 09:10:25 -0300

    feat: issue 15

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"github.com/csouzape/dsxconfig/tui"
 )
 
+const version = "1.0.0"
+
 func main() {
+	if len(os.Args) > 1 && os.Args[1] == "--version" {
+		fmt.Println(version)
+		os.Exit(0)
+	}
+
 	if err := tui.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
